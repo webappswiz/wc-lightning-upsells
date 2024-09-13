ad176ccc4cf55ecda1a8acbbbcdef6049758bc60
artem <artem.the.freelancer@gmail.com>
2024-09-13 20:17:37 +0400

    v0.1.2 - Added calculations for all price types. Implemented {offered_price} and {product_price} form tags

--- inc/class-wclu-upsell-offer.php
+++ inc/class-wclu-upsell-offer.php
@@ -12,18 +12,22 @@ class Wclu_Upsell_Offer extends Wclu_Core {
   public $title;
   public $content;
   
-  // these properties are stores in wp_postmeta table, and we handle them by ourselves.
+  // these properties are stored in wp_postmeta table, and we handle them by ourselves.
   // default values for these properties are set in Wclu_Core::$default_upsell_settings
   
   public $product_id;
-  public $price_type;
+  public $price_type; // must be one of Wclu_Core::PRICE_TYPE_XXXXXX
   public $offered_price;
+  
+  private $regular_product_price;
   
   // form tags available inside upsell content
   
   const FORM_TAGS = array(
     'accept',
-    'skip'
+    'skip',
+    'offered_price',
+    'product_price'
   );
   
   /**
@@ -47,6 +51,9 @@ class Wclu_Upsell_Offer extends Wclu_Core {
         $this->$key = $default_value;
       }
     }
+    
+    $this->regular_product_price = $this->get_product_price();
+    
   }
   
   /**
@@ -75,8 +82,12 @@ class Wclu_Upsell_Offer extends Wclu_Core {
     
     foreach ( self::FORM_TAGS as $form_tag ) {
       
+      $search_for = '{' . $form_tag . '}';
       $function_name = 'render_tag_' . $form_tag;
-      $prepared_content = str_replace( '{' . $form_tag . '}', $this->$function_name(), $prepared_content );
+      
+      if ( strpos( $this->content, $search_for ) !== false ) {
+        $prepared_content = str_replace( $search_for, $this->$function_name(), $prepared_content );
+      }
       
     }
     
@@ -99,6 +110,97 @@ class Wclu_Upsell_Offer extends Wclu_Core {
   private function render_tag_skip() {
     $out = '#skip_lightning_' . $this->id;
     return $out;
+  }
+  
+  /**
+   * Used by get_prepared_content()
+   * @return string
+   */
+  private function render_tag_offered_price() {
+    
+    $price = $this->calculate_offered_price();
+    
+    if ( floor( $price ) == $price ) { // special case to avoid printing numbers like 1050.00 and print just 1050
+      $decimals = 0;
+    }
+    else {
+      $decimals = wc_get_price_decimals();
+    }
+    
+    $out = wc_price( $price, array( 'decimals' => $decimals ) ); 
+    
+    return $out;
+  }
+  
+  /**
+   * Used by get_prepared_content()
+   * @return string
+   */
+  private function render_tag_product_price() {
+    
+    $price = $this->regular_product_price;
+    
+    if ( floor( $price) == $price ) { // special case to avoid printing numbers like 1050.00 and print just 1050
+      $decimals = 0;
+    }
+    else {
+      $decimals = wc_get_price_decimals();
+    }
+    
+    $out = wc_price( $price, array( 'decimals' => $decimals ) );
+    
+    return $out;
+  }
+  
+  /**
+   * Get the regular price of the product which is offered in this upsell.
+   * 
+   * @return float
+   */
+  private function get_product_price() {
+    
+    $price = 0.0;
+    
+    $pf = new WC_Product_Factory();
+    $product = $pf->get_product( $this->product_id ); 
+    if ( is_object( $product ) ) {
+      $price = $product->get_regular_price();
+    }
+    
+    return $price;
+  }
+  
+  /**
+   * Get the discounted price which is offered in this upsell.
+   * 
+   * @return float
+   */
+  public function calculate_offered_price() {
+    
+    $result = $this->regular_product_price;
+    
+    switch( $this->price_type ) {
+      
+      case self::PRICE_TYPE_DISCOUNT:
+        
+        $result = $this->regular_product_price - $this->offered_price;
+        break;
+    
+      case self::PRICE_TYPE_PERCENT_DISCOUNT:
+        
+        $discount = $this->offered_price > 100 ? 1 : ( $this->offered_price / 100 );
+        
+        $result = ( 1 - $discount ) * $this->regular_product_price;
+        break;
+    
+      case self::PRICE_TYPE_FIXED:
+      default:
+        
+        $result = $this->offered_price;
+        break;
+    }
+    
+    return $result;
   }
   
 }
